473e1d46c59e13c835b31835fb04b0e2b2492927
Henry Nwoke <nwokehenry90@outlook.com>
2025-11-19 15:47:08 -0500

    Trigger rebuild

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Nwoke ECS Container."
+    return "Hello from Nwoke ECS Container. Version 2"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
